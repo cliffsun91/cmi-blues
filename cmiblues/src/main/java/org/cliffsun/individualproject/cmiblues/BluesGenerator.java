e677faebca491661484bd505fcc6e05592d59060
cliffsun91 <cliffsun91@gmail.com>
2012-11-14 23:14:53 +0000

    Created Maven Project
    Created barebones Maven project which allows for quick
    building/testing/deployment

---
+++ cmiblues/src/main/java/org/cliffsun/individualproject/cmiblues/BluesGenerator.java
@@ -0,0 +1,16 @@
+package org.cliffsun.individualproject.cmiblues;
+
+public class BluesGenerator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Will generate 4 bars of blues initially
+		// Will use a grammar and a parse tree 
+		// Forming a line of music like forming a sentence of words
+		System.out.println("helloworld");
+	}
+
+}
