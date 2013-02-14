01741c58fc237ff4cd6d5cdd2fad3c4c464b4422
cliffsun91 <cliffsun91@gmail.com>
2013-02-14 13:34:19 +0000

    can generate actual notes from the terminal strings
    termina parser uses tone to note converter to produce suitable notes.

--- cmiblues/src/main/java/org/cliffsun/individualproject/grammar/Example.java
+++
@@ -1,8 +0,0 @@
-package org.cliffsun.individualproject.grammar;
-
-public interface Example {
-	public String[] generate(String phrase);
-	public String[] generateTree(String phrase);
-	//public String[] mappend(String functionPointer, String phrase);
-
-}
